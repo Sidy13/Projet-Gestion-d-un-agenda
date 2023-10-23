0e7903894f9c7234c2bc144e9d1956407056f222
Sidy13 <sidydcr.pro@gmail.com>
2023-10-23 14:56:21 +0200

    Création des structures

--- fichier.c
+++ fichier.c
@@ -2,6 +2,8 @@
 // Created by Sidy on 23/10/2023.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "fichier.h"
 
-b
+
--- fichier.h
+++ fichier.h
@@ -4,7 +4,21 @@
 
 #ifndef PROJET_GESTION_D_UN_AGENDA_FICHIER_H
 #define PROJET_GESTION_D_UN_AGENDA_FICHIER_H
+#include <stdio.h>
+#include <stdlib.h>
 
 
-h
+typedef struct s_d_cell
+{
+    int value;
+    struct s_d_cell *next_0;
+    struct s_d_cell *next_1;
+} t_d_cell;
+
+typedef struct s_d_list
+{
+    t_d_cell *head_0;
+    t_d_cell *head_1;
+} t_d_list;
+
 #endif //PROJET_GESTION_D_UN_AGENDA_FICHIER_H
