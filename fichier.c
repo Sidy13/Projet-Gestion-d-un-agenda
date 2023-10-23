3b60c8ff9fcf5945b8f0b306a85ed174aeaaf703
Sidy13 <sidydcr.pro@gmail.com>
2023-10-23 15:33:03 +0200

    Création des structures

--- fichier.c
+++ fichier.c
@@ -6,4 +6,9 @@
 #include <stdlib.h>
 #include "fichier.h"
 
-
+t_d_cell CreateCell(int val, int level){
+    t_d_cell cell;
+    cell.value = val;
+    int *niveau = (int*)malloc(sizeof(int));
+    
+}
