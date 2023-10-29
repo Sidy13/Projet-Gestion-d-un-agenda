4a0ba923a9e0a48152a5f1ef563769cd897bd34c
Sidy13 <sidydcr.pro@gmail.com>
2023-10-29 21:53:23 +0100

    Correction de createcell et création d'emptylist

--- fichier.c
+++ fichier.c
@@ -7,15 +7,22 @@
 #include "fichier.h"
 
 t_d_cell CreateCell(int val, int level){
-    t_d_cell *cell = (t_d_cell*)malloc(sizeof(t_d_cell));
-    cell->value = val;
-    cell->level = level;
+    t_d_cell cell;
+    cell.value = val;
+    cell.level = level;
+    cell.next = (t_d_cell**)malloc(level * sizeof(t_d_cell*));
     for (int i = 0; i <level; i++){
-        cell->next[i] = NULL;
+        cell.next[i] = NULL;
     }
-    return *cell;
+    return cell;
 }
 
 t_d_list emptylist(int max_level){
-
+    t_d_list list;
+    list.head = (t_d_cell**) malloc(sizeof(t_d_cell));
+    list.hauteur = max_level;
+    for (int i = 0; i<max_level; i++){
+        list.head[i] = NULL;
+    }
+    return list;
 }
