d9fbc160a17513fc1a475ec18cdb62f572d70604
Sidy13 <sidydcr.pro@gmail.com>
2023-10-30 11:39:19 +0100

    Création de display_cell_list

--- fichier.c
+++ fichier.c
@@ -10,7 +10,7 @@ t_d_cell CreateCell(int val, int level){
     t_d_cell cell;
     cell.value = val;
     cell.level = level;
-    cell.next = (t_d_cell**)malloc(level * sizeof(t_d_cell*));
+    cell.next = (t_d_cell**)malloc((level+1) * sizeof(t_d_cell*));
     for (int i = 0; i <level; i++){
         cell.next[i] = NULL;
     }
@@ -19,7 +19,7 @@ t_d_cell CreateCell(int val, int level){
 
 t_d_list emptylist(int max_level){
     t_d_list list;
-    list.head = (t_d_cell**) malloc(sizeof(t_d_cell));
+    list.head = (t_d_cell**) malloc((max_level+1) * sizeof(t_d_cell));
     list.hauteur = max_level;
     for (int i = 0; i<max_level; i++){
         list.head[i] = NULL;
@@ -27,10 +27,20 @@ t_d_list emptylist(int max_level){
     return list;
 }
 
-t_d_list addcell_headlist(t_d_cell cell,t_d_list list, int max_level){
+t_d_list *addcell_headlist(t_d_cell *cell,t_d_list *list, int max_level){
     for(int i = 0; i<max_level; i++){
-        list.head[i] = list.head[i+1];
+        list->head[i] = list->head[i+1];
     }
-    list.head[0] = &cell;
+    list->head[0] = cell;
     return list;
+}
+
+void display_cell_list(t_d_list list, int level) {
+    if (level < list.hauteur) {
+        t_d_cell *current = list.head[level];
+        while (current != NULL) {
+            printf("%d ", current->value);
+            current = current->next[level];
+        }
+    }
 }
--- fichier.h
+++ fichier.h
@@ -24,5 +24,6 @@ typedef struct s_d_list
 
 t_d_cell CreateCell();
 t_d_list emptylist(int max_level);
-t_d_list addcell_headlist(t_d_cell cell,t_d_list list, int max_level);
+t_d_list *addcell_headlist(t_d_cell *cell,t_d_list *list, int max_level);
+void display_cell_list(t_d_list list, int level);
 #endif //PROJET_GESTION_D_UN_AGENDA_FICHIER_H
