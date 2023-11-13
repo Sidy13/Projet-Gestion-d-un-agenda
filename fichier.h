9fe3231390ce83d57011da38a45be0bff88f1afe
Sidy13 <sidydcr.pro@gmail.com>
2023-11-13 15:35:00 +0100

    Création de nouvelles structures

--- fichier.h
+++ fichier.h
@@ -21,9 +21,30 @@ typedef struct s_d_list
     int hauteur;
 } t_d_list;
 
+
+
+typedef struct
+t_d_cell CreateCell(int val, int level);
+t_d_list emptylist(int max_level);
+t_d_list *addcell_headlist(t_d_cell *cell,t_d_list *list, int max_level);
+void display_cell_list(t_d_list list, int level);
+void display_all_level(t_d_list list, int level);
+void display_aligned_cell(t_d_cell cell, t_d_list list, int level);
+void addcell_anywhere_inlist(t_d_list* list, t_d_cell *cell);
+int search_at_zero(t_d_list list, int val);
+int search_from_top(t_d_list list, int val);
+
+//Partie 2
+
+int puissance_deux (int power);
+t_d_list create_tab(int power);
+
+
+//Partie 3
+
 typedef struct{
     char* nom;
-     char *prenom[50];
+    char *prenom[50];
 } contact;
 
 typedef struct{
@@ -49,22 +70,6 @@ typedef struct{
     duree Duree;
     char* objet;
 } rdv;
-
-typedef struct
-t_d_cell CreateCell(int val, int level);
-t_d_list emptylist(int max_level);
-t_d_list *addcell_headlist(t_d_cell *cell,t_d_list *list, int max_level);
-void display_cell_list(t_d_list list, int level);
-void display_all_level(t_d_list list, int level);
-void display_aligned_cell(t_d_cell cell, t_d_list list, int level);
-void addcell_anywhere_inlist(t_d_list* list, t_d_cell *cell);
-int search_at_zero(t_d_list list, int val);
-int search_from_top(t_d_list list, int val);
-
-//Partie 2
-
-int puissance_deux (int power);
-t_d_list create_tab(int power);
 
 
 #endif //PROJET_GESTION_D_UN_AGENDA_FICHIER_H
